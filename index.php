5ada7332934e8316ce3784bc80fe7a91e5fc4b87
Shantanu Singh <47878607+Shantanu2k19@users.noreply.github.com>
2021-04-02 15:08:13 +0530

    updated guess number to 72

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
                 echo("Your guess is too short");
             } else if ( ! is_numeric($_GET['guess']) ) {
                 echo("Your guess is not a number");
-            } else if ( $_GET['guess'] < 46 ) {
+            } else if ( $_GET['guess'] < 72 ) {
                 echo("Your guess is too low");
-            } else if ( $_GET['guess'] > 46 ) {
+            } else if ( $_GET['guess'] > 72 ) {
                 echo("Your guess is too high");
             } else {
                 echo("Congratulations - You are right");
